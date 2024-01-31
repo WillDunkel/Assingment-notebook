721572de2e220fd26b4b31a3771e3ff9b70d19cd
William Dunkel <wndunkel@gmail.com>
2024-01-31 09:39:23 -0600

    Initial Commit

---
+++ Assingment notebook/Assingment_notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assingment_notebookApp.swift
+//  Assingment notebook
+//
+//  Created by William Dunkel on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Assingment_notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
