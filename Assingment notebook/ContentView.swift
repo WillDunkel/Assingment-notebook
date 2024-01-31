83163253b39a66ca28399dcc11a79691e77d9787
William Dunkel <wndunkel@gmail.com>
2024-01-31 09:51:50 -0600

    did first 2 assignment

--- Assingment notebook/ContentView.swift
+++ Assingment notebook/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDoItems =
+            [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+             ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+             ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(toDoItems){ item in
+                    Text(item.description)
+                }
+                .onMove { indices, newOffset in
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    toDoItems.remove(atOffsets: indexSet)
+                }
+            }
+            .navigationBarTitle("To Do List")
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
@@ -23,4 +35,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
 }
